74c60823814ca1136f7e9f82a87c045e37698356
HARIOM-JHA01 <96457683+HARIOM-JHA01@users.noreply.github.com>
2023-07-01 11:42:46 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ document.querySelector("button").addEventListener("click", function () {
   var x = "./images/dice" + number1 + ".png";
   document.querySelector(".img1").setAttribute("src", x);
   // Random Number Generation for Image2
+
   var number2 = Math.floor(Math.random() * 6 + 1);
   var y = "./images/dice" + number2 + ".png";
   document.querySelector(".img2").setAttribute("src", y);
@@ -12,6 +13,6 @@ document.querySelector("button").addEventListener("click", function () {
   if (number1 > number2)
     document.querySelector("h1").textContent = "🚩 Player 1 won";
   else if (number1 === number2)
-    document.querySelector("h1").textContent = "Draw";
+    document.querySelector("h1").textContent = "🚩 Draw";
   else document.querySelector("h1").textContent = "🚩 Player 2 Won";
 });
